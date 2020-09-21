5b6ca1492266e550c7fc4fabae6668e2651298e2
nicholasdotsol <nicholas.evans@thesis.co>
2020-09-21 20:03:04 +0000

    fix obj shorthand property

--- scriptConfig.js
+++ scriptConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
     operator: "0x39d2aCBCD80d80080541C6eed7e9feBb8127B2Ab",
     url: "https://eth-mainnet.alchemyapi.io/v2/6Euy0LCtPGXsd5fnPe3Er4CZ7bEgwd6w",
-    pagerdutyApiToken = "yourapiTokenGoesHere"
+    pagerdutyApiToken: "yourapiTokenGoesHere"
   }
