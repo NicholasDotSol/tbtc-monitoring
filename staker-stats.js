83d7923c19c2bad571a5ea12d6538a9ae8d9b3f1
nicholasdotsol <nicholas.evans@thesis.co>
2020-09-24 09:54:17 +0000

    Only reginstered keeps option
    Pass any command line argument to get only reginstered keeps

--- staker-stats.js
+++ staker-stats.js
@@ -25,8 +25,14 @@ async function main() {
 		for (let op of operators) {
 			const kbal = op[1]
       		const available = await keepBondingContract.unbondedValue(op[0])
-      		const isRegistered = await bondedECDSAKeepFactory.isOperatorRegistered(op[0], "0xe20A5C79b39bC8C363f0f49ADcFa82C2a01ab64a")
-			console.log(`${op[0]} stakes ${ethers.utils.formatEther(kbal)} KEEP + ${parseFloat(ethers.utils.formatEther(available)).toFixed(2)} ETH unbonded - registered ${isRegistered}`)
+			  const isRegistered = await bondedECDSAKeepFactory.isOperatorRegistered(op[0], "0xe20A5C79b39bC8C363f0f49ADcFa82C2a01ab64a")
+			  if(process.argv.length > 2){
+				if(isRegistered){
+					console.log(`${op[0]} stakes ${ethers.utils.formatEther(kbal)} KEEP + ${parseFloat(ethers.utils.formatEther(available)).toFixed(2)} ETH unbonded - registered ${isRegistered}`)
+				  }
+			  }else{
+				console.log(`${op[0]} stakes ${ethers.utils.formatEther(kbal)} KEEP + ${parseFloat(ethers.utils.formatEther(available)).toFixed(2)} ETH unbonded - registered ${isRegistered}`)
+			  }
     		  eStakedAll = eStakedAll.add(available)
      		 if (isRegistered) {
        			kStaked = kStaked.add(kbal)
